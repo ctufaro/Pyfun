c93f92d351a71054742b80978669d45c1bece587
unknown <ctufaro@gmail.com>
2017-06-27 15:52:10 -0400

    Data file dump

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -1,5 +1,15 @@
+import os
 import numpy as np 
 import matplotlib.pyplot as plt
 import pandas as pd 
+
+# Import the dataset
+path = 'C:\\Users\\ctufaro_admin\\My Projects\\Pyfun\Machine-Learning-A-Z-Template-Folder\\Machine Learning A-Z\\Part 1 - Data Preprocessing'
+pathData = path + '\\Data.csv'
+
+dataset = pd.read_csv(pathData)
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 3].values
+
 
 

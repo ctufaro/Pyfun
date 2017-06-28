28976310b7f91e42a49fedd00eefbc5587937588
unknown <ctufaro@gmail.com>
2017-06-28 16:56:46 -0400

    Linear Regression

--- data_preprocessing_template_org.py
+++ data_preprocessing_template_org.py
@@ -11,12 +11,30 @@ dataset = pd.read_csv(pathData)
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 3].values
 
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encode categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
+
 #splitting the test set and training set
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
 #feature scaling
-'''from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 x_train = sc_X.fit_transform(x_train)
-x_test = sc_X.transform(x_test)'''
+x_test = sc_X.transform(x_test)
+
+
+
--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -1,11 +1,11 @@
-import os
+# Simple Linear Regression
 import numpy as np 
 import matplotlib.pyplot as plt
 import pandas as pd 
 
 # Import the dataset
-path = 'C:\\Users\\ctufaro_admin\\My Projects\\Pyfun\Machine-Learning-A-Z-Template-Folder\\Machine Learning A-Z\\Part 1 - Data Preprocessing'
-pathData = path + '\\Data.csv'
+path = 'C:\\Users\\ctufaro_admin\\My Projects\\Pyfun\Machine-Learning-A-Z-Template-Folder\\Machine Learning A-Z\\Part 2 - Regression\\Section 4 - Simple Linear Regression'
+pathData = path + '\\Salary_Data.csv'
 
 dataset = pd.read_csv(pathData)
 X = dataset.iloc[:, :-1].values
@@ -20,3 +20,4 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, rando
 sc_X = StandardScaler()
 x_train = sc_X.fit_transform(x_train)
 x_test = sc_X.transform(x_test)'''
+

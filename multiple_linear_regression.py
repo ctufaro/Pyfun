35dd796936798b030073e2a4efc0811e6aded1a6
christufaro <ctufaro@gmail.com>
2017-07-12 08:00:54 -0400

    Polynomial Regression

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd 
 # Import the dataset
-path = 'C:\\Users\\ctufaro_admin\\My Projects\\Pyfun\Machine-Learning-A-Z-Template-Folder\\Machine Learning A-Z\\Part 2 - Regression\\Section 5 - Multiple Linear Regression\\Multiple_Linear_Regression'
+path = 'C:\\Users\\Chris\\My Projects\\Pyfun\Machine-Learning-A-Z-Template-Folder\\Machine Learning A-Z\\Part 2 - Regression\\Section 5 - Multiple Linear Regression\\Multiple_Linear_Regression'
 pathData = path + '\\50_Startups.csv'
 
 dataset = pd.read_csv(pathData)
@@ -18,26 +18,53 @@ X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
 
-#avoiding the dummy variable trap
+# Avoiding the dummy variable trap
 X = X[:, 1:]
 
-#splitting the test set and training set
+# Splitting the test set and training set
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
-#feature scaling
+# Feature scaling
 '''from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 x_train = sc_X.fit_transform(x_train)
 x_test = sc_X.transform(x_test)'''
 
-#Fitting Multiple Linear Regression to the Training Set
+# Fitting Multiple Linear Regression to the Training Set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(x_train, y_train)
 
-#Predicting the test set results
+# Predicting the test set results
 y_pred = regressor.predict(x_test)
 
-#Building backward elimination
-import statsmodels.formula api as sm
+# Building the optimal model using Elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50,1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0,1,2,3,4,5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0,1,3,4,5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0,3,4,5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0,3,5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0,3]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+
+
+
+
+
---
+++ polynomial_regression.py
@@ -0,0 +1,7 @@
+#polynomial linear regression
+import numpy as np 
+import matplotlib.pyplot as plt
+import pandas as pd 
+# Import the dataset
+path = 'C:\\Users\\Chris\\My Projects\\Pyfun\Machine-Learning-A-Z-Template-Folder\\Machine Learning A-Z\\Part 2 - Regression\\Section 6 - Polynomial Regression'
+pathData = path + '\\Position_Salaries.csv'
